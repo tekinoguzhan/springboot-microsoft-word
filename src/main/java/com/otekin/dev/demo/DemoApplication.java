18e9aba8d98d4446a0f669dc04a4ecc75eb19e3c
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-09-01 05:57:44 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/dev/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.otekin.dev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgefactorydemoApplication.class, args);
+	}
+
+}
